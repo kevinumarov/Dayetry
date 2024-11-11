7de16863a6b25fb040fda5dcade2585594ec2dbf
kevinumarov <keulifeinfluencer@gmail.com>
2024-11-11 20:40:50 +0900

    Initial Commit

---
+++ Dayetry/DayetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayetryApp.swift
+//  Dayetry
+//
+//  Created by Kevin Umarov on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DayetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
